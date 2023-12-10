33cbab6cc6a865f59ae276772d5aa83394d9090c
youmitsu <holly.app.dev@gmail.com>
2023-12-10 22:57:58 +0900

    fix: set textfield background color

--- feature/registration/src/main/java/com/example/registration/RegistrationRoute.kt
+++ feature/registration/src/main/java/com/example/registration/RegistrationRoute.kt
@@ -1,14 +1,15 @@
 package com.example.registration
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -50,17 +51,24 @@ fun RegistrationUiHost(
         }
     ) {
         Box(
-            modifier = Modifier.padding(it)
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
         ) {
-            Column {
-                TextField(
-                    value = state.title,
-                    onValueChange = { value ->
-                        onUpdateTitle(value)
-                    },
-                    modifier = Modifier.background(Color.Transparent)
-                )
-            }
+            TextField(
+                value = state.title,
+                onValueChange = { value ->
+                    onUpdateTitle(value)
+                },
+                modifier = Modifier
+                    .fillMaxSize(),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                ),
+                placeholder = {
+                    Text("メモしたい名言を入力", color = MaterialTheme.colorScheme.secondary)
+                }
+            )
         }
     }
 }
