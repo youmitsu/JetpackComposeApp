ffa39f79b15e2fbbd72ca6efeffe1cec3d44cc2c
youmitsu <holly.app.dev@gmail.com>
2024-01-12 22:33:37 +0900

    fix: list UI

--- feature/list/src/main/java/com/example/feature/list/ui/ListRoute.kt
+++ feature/list/src/main/java/com/example/feature/list/ui/ListRoute.kt
@@ -1,18 +1,17 @@
 package com.example.feature.list.ui
 
+import android.content.res.Configuration
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -31,31 +30,40 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.feature.list.R
+import com.example.ui.theme.BaseAppTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun ListRoute() {
-    ListScreen()
+fun ListRoute(
+    listViewModel: ListViewModel = hiltViewModel()
+) {
+    val uiState by listViewModel.uiState.collectAsState()
+
+    ListScreen(
+        onRefresh = listViewModel::refresh,
+        uiState = uiState,
+    )
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 internal fun ListScreen(
+    uiState: ListUiState,
+    onRefresh: () -> Unit,
     modifier: Modifier = Modifier,
-    listViewModel: ListViewModel = hiltViewModel()
 ) {
-    val uiState by listViewModel.uiState.collectAsState()
     val state = rememberPullRefreshState(
         refreshing = uiState.isRefreshing,
-        onRefresh = { listViewModel.refresh() }
+        onRefresh = onRefresh
     )
-
     Box(modifier = modifier.pullRefresh(state)) {
         LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
             items(items = uiState.currentItems) { item ->
-                Meigen(name = item.body)
+                Meigen(title = item.body)
             }
         }
 
@@ -68,7 +76,7 @@ internal fun ListScreen(
 }
 
 @Composable
-private fun Meigen(name: String, modifier: Modifier = Modifier) {
+private fun Meigen(title: String, modifier: Modifier = Modifier) {
     var expanded by remember {
         mutableStateOf(false)
     }
@@ -85,49 +93,50 @@ private fun Meigen(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(
-                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = title,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
                 )
-                if (expanded) {
-                    Text(
-                        text = ("Composem ipsum color sit lazy, " +
-                                "padding theme elit, sed do bouncy. ").repeat(4),
-                    )
-                }
-            }
-            IconButton(onClick = { expanded = !expanded }) {
+            )
+            IconButton(onClick = {}) {
                 Icon(
-                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                    contentDescription = if (expanded) {
-                        stringResource(R.string.show_less)
-                    } else {
-                        stringResource(R.string.show_more)
-                    }
+                    imageVector = Icons.Filled.Edit,
+                    contentDescription = stringResource(R.string.edit)
                 )
             }
         }
     }
 }
 
-//@Preview(
-//    showBackground = true,
-//    widthDp = 320,
-//    uiMode = Configuration.UI_MODE_NIGHT_YES,
-//    name = "Dark"
-//)
-//@Preview(showBackground = true, widthDp = 320)
-//@Composable
-//fun GreetingPreview() {
-//    BaseAppTheme {
-//        MeigenList()
-//    }
-//}
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    BaseAppTheme {
+        ListScreen(
+            uiState = ListUiState(
+                currentItems = listOf(
+                    com.example.model.Meigen(
+                        id = "1",
+                        body = "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello",
+                        createdAt = java.util.Date()
+                    ),
+                    com.example.model.Meigen(
+                        id = "2",
+                        body = "World",
+                        createdAt = java.util.Date()
+                    ),
+                )
+            ),
+            onRefresh = {},
+        )
+    }
+}
